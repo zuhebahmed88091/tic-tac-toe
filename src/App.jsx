c94632edad37dd0d5122270df0e89b20cdcbeace
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2024-01-17 13:23:34 +0600

    Setup the sqaures element code primarily

--- src/App.jsx
+++ src/App.jsx
@@ -18,28 +18,30 @@ function Square({ value, onSquareClick }) {
 export default function Board() {
   const [ squares, setSquares ] = useState(Array(9).fill(null));
 
-  function handleClick () {
-    console.log('click');
+  function handleClick (index) {
+    const nextSquares = squares.slice();
+    nextSquares[index] = "X";
+    setSquares(nextSquares);
   }
 
   return (
     <>
       <div className="flex">
-        <Square value={squares[0]} onSquareClick={handleClick}/>
-        <Square value={squares[1]}/>
-        <Square value={squares[2]}/>
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
       </div>
 
       <div className="flex">
-        <Square value={squares[3]}/>
-        <Square value={squares[4]}/>
-        <Square value={squares[5]}/>
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
       </div>
 
       <div className="flex">
-        <Square value={squares[6]}/>
-        <Square value={squares[7]}/>
-        <Square value={squares[8]}/>
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
       </div>
     </>
   );
