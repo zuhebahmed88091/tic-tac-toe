4690d23b5d58cd448a2be2815ec36eb1ed24c167
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2024-01-17 12:24:36 +0600

    Created square component

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,30 @@
-export default function Square() {
+function Square() {
+  return (
+    <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">
+      X
+    </button>
+  );
+}
+
+export default function Board() {
   return (
     <>
       <div>
-        <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">
-          X
-        </button>
-        <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">
-          X
-        </button>
-        <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">
-          X
-        </button>
+        <Square />
+        <Square />
+        <Square />
       </div>
 
       <div>
-        <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">
-          X
-        </button>
-        <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">
-          X
-        </button>
-        <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">
-          X
-        </button>
+        <Square />
+        <Square />
+        <Square />
       </div>
 
       <div>
-        <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">
-          X
-        </button>
-        <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">
-          X
-        </button>
-        <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">
-          X
-        </button>
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
