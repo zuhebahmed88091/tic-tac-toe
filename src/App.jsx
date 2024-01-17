1d3b812934e8699a7fbd117bfdc652d9bde814d8
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2024-01-17 13:09:37 +0600

    Set the lifting state up

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react";
 
-function Square() {
-  const [ value, setValue ] = useState(null);
+function Square({ value, onSquareClick }) {
 
-  function handleClick() {
-    setValue("X");
-  }
+
 
   return (
     <button
       className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg"
-      onClick={handleClick}
+      onClick={onSquareClick}
     >
       {value}
     </button>
@@ -19,24 +16,30 @@ function Square() {
 }
 
 export default function Board() {
+  const [ squares, setSquares ] = useState(Array(9).fill(null));
+
+  function handleClick () {
+    console.log('click');
+  }
+
   return (
     <>
       <div className="flex">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} onSquareClick={handleClick}/>
+        <Square value={squares[1]}/>
+        <Square value={squares[2]}/>
       </div>
 
       <div className="flex">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]}/>
+        <Square value={squares[4]}/>
+        <Square value={squares[5]}/>
       </div>
 
       <div className="flex">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]}/>
+        <Square value={squares[7]}/>
+        <Square value={squares[8]}/>
       </div>
     </>
   );
