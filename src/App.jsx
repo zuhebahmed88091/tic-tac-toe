fe62c1918280132ccc7992c4ab0582bc4568f429
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2024-01-17 12:42:47 +0600

    Setup primary handle click

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
-function Square({ value }) {
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+
+function Square() {
+  const [ value, setValue ] = useState(null);
+
+  function handleClick() {
+    setValue("X");
+  }
+
   return (
-    <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">
+    <button
+      className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg"
+      onClick={handleClick}
+    >
       {value}
     </button>
   );
@@ -10,21 +22,21 @@ export default function Board() {
   return (
     <>
       <div>
-        <Square value="1"/>
-        <Square value="2"/>
-        <Square value="3"/>
+        <Square />
+        <Square />
+        <Square />
       </div>
 
       <div>
-        <Square value="4"/>
-        <Square value="5"/>
-        <Square value="6"/>
+        <Square />
+        <Square />
+        <Square />
       </div>
 
       <div>
-        <Square value="7"/>
-        <Square value="8"/>
-        <Square value="9"/>
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
