e7bcad4e7ab8e41a65ef2e1887dde3bd5a2c977b
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2024-01-17 12:29:51 +0600

    sending props in square component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-function Square() {
+function Square({ value }) {
   return (
     <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">
-      X
+      {value}
     </button>
   );
 }
@@ -10,21 +10,21 @@ export default function Board() {
   return (
     <>
       <div>
-        <Square />
-        <Square />
-        <Square />
+        <Square value="1"/>
+        <Square value="2"/>
+        <Square value="3"/>
       </div>
 
       <div>
-        <Square />
-        <Square />
-        <Square />
+        <Square value="4"/>
+        <Square value="5"/>
+        <Square value="6"/>
       </div>
 
       <div>
-        <Square />
-        <Square />
-        <Square />
+        <Square value="7"/>
+        <Square value="8"/>
+        <Square value="9"/>
       </div>
     </>
   );
